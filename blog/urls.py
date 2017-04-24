faee9ace6963dcbf32eebaa6f0c2e4c16fe0a574
Tony <antoniocorregedor@gmail.com>
2017-04-24 16:34:23 +0200

    Added view and template for detailed blog post as well as CSS for the site.

--- blog/urls.py
+++ blog/urls.py
@@ -8,6 +8,13 @@ urlpatterns = [
 	#That's correct, because in Django URL resolvers, 'http://127.0.0.1:8000/' is not a part of the URL. 
 	#This pattern will tell Django that views.post_list is the right place to go if someone enters website at the 'http://127.0.0.1:8000/' address.
     url(r'^$', views.post_list, name='post_list'),  
+	
+	#it starts with ^ again – "the beginning".
+	#post/ just means that after the beginning, the URL should contain the word post and a /. So far so good.
+	#(?P<pk>\d+) – this part is trickier. It means that Django will take everything that you place here and transfer it to a view as a variable called pk. (Note that this matches the name we gave the primary key variable back in blog/templates/blog/post_list.html!) \d also tells us that it can only be a digit, not a letter (so everything between 0 and 9). + means that there needs to be one or more digits there. So something like http://127.0.0.1:8000/post// is not valid, but http://127.0.0.1:8000/post/1234567890/ is perfectly OK!
+	#/ – then we need a / again.
+	#$ – "the end"!
+	url(r'^post/(?P<pk>\d+)/$', views.post_detail, name='post_detail'),
 
 ]#End URL Patterns
 
--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 #The dot before models means current directory or current application. Both views.py and models.py are in the same directory. 
 #This means we can use . and the name of the file (without .py). Then we import the name of the model (Post).
@@ -11,5 +11,8 @@ from .models import Post
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')  #Name of our query set
     return render(request, 'blog/post_list.html', {'posts': posts})
-	
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
 	
